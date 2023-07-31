8fa25342fd0279d19199a182ec76465f6610b499
Binjiho <jiyu1948@gmail.com>
2023-07-31 21:45:34 +0900

    Initial commit

---
+++ src/main/java/com/example/brave_webtoon/BraveWebtoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.brave_webtoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraveWebtoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BraveWebtoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/brave_webtoon/BraveWebtoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brave_webtoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraveWebtoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
